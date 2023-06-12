4622c27aea4c71764508bdc234b6fe5af3434b01
Fidelis0001 <polina.rykova25@gmail.com>
2023-06-12 20:13:39 +0200

    cleaner code + padding adjustment

--- index.js
+++ index.js
@@ -6,28 +6,14 @@ guestScore.textContent = 0;
 
 let countHome = 0;
 let countGuest = 0;
-function addOneHome() {
-  countHome += 1;
+
+function addToHome(count) {
+  countHome += count;
   homeScore.textContent = countHome;
 }
-function addTwoHome() {
-  countHome += 2;
-  homeScore.textContent = countHome;
-}
-function addThreeHome() {
-  countHome += 3;
-  homeScore.textContent = countHome;
-}
-function addOneGuest() {
-  countGuest += 1;
-  guestScore.textContent = countGuest;
-}
-function addTwoGuest() {
-  countGuest += 2;
-  guestScore.textContent = countGuest;
-}
-function addThreeGuest() {
-  countGuest += 3;
+
+function addToGuest(count2) {
+  countGuest += count2;
   guestScore.textContent = countGuest;
 }
 
